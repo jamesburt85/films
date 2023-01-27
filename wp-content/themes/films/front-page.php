be2ff01f7685703fd565ccbadf769342d54e2983
AD <ad@adstyl.es>
2023-01-27 09:31:39 +0000

    porting acf to taxonomy

--- wp-content/themes/films/front-page.php
+++ wp-content/themes/films/front-page.php
@@ -4,14 +4,55 @@
 		
 		<?php 
 
-
-			// AD NOTE TO AD - 26th JAN...
 			// need to loop through kit, get kit category ACF, then add it to kit category TAXONOMY, and also get that kit categories sub category ACF (e.g. lenses), and add that in as a child taxonomy of parent tax.
 
-			// maybe we just add all the taxonomies in, and then organise the actual taxonomy afterwards. So just chuck all tax in at parent level. And then manually restrucutre the category afterwards
+			// get all kit taxonomies and their IDs as we'll need them...
+
+			$kit_cats = get_terms( array(
+			    'taxonomy' => 'kit_category',
+			    'hide_empty' => false,
+			) );
+
+			// echo "<pre>";
+			// print_r($kit_cats);
+			// echo "</pre>";
+
+			$arrayOfCats = array();
+
+			foreach ($kit_cats as $key => $value) {
+			// 	// code...
+			// }
+			// foreach ($kit_cats as $katcat) {
+
+				// echo "<pre>";
+				// print_r($key);
+				// echo "<br/>";
+				// print_r($value);
+				// echo "</pre>";
+
+				$slug = $value->slug;
+				$term_id = $value->term_id;
+				echo "<pre>";
+				print_r($slug);
+				echo "<br/>";
+				print_r($term_id);
+				echo "</pre>";
+
+				// $arrayOfCats = array();
+
+				$arrayOfCats[$slug] = $term_id;
+				// array_push($arrayOfCats, array($slug, $term_id));
+				// $arrayOfCats[$key] = $value[$term_id];
+
+			}
+
+			echo "<pre>";
+			print_r($arrayOfCats);
+			echo "</pre>";
+
 
 			$args = array(
-			    'post_type'    => 'kit',
+			    'post_type'    => 'kit', // people
 			    // 'title_li' => ''
 			    // 'order' => 'ASC',
 			    'posts_per_page' => -1,
@@ -31,22 +72,69 @@
 				        $the_query->the_post(); ?>
 				        <h3><?php the_title(); ?></h3>
 				        <?php
-				        	$tag = get_field( "category" );
+				        	$kit_category = get_field( "kit_category" );
+				        	$solid_state_category = get_field( "solid_state_category" );
+				        	$accessory_category = get_field( "accessory_category" );
+				        	$lens_category = get_field( "lens_category" );
 							// print_r($tag);
+
+							// now get the IDs from any categories item has, by checking the above array // $arrayOfCats
+
+
+
+							echo "<pre>";
+							print_r($kit_category);
+							echo "<br/>";
+							print_r($solid_state_category);
+							echo "<br/>";
+							print_r($accessory_category);
+							echo "<br/>";
+							print_r($lens_category);
+							echo "</pre>";
+
+							$TaxIDsToBeAdded = [];
+
+							if (!empty($kit_category)) {
+								$kit_cat_ID = $arrayOfCats[$kit_category];
+								$TaxIDsToBeAdded[] = $kit_cat_ID;
+							}
+
+							if (!empty($solid_state_category)) {
+								$kit_cat_ID = $arrayOfCats[$solid_state_category];
+								$TaxIDsToBeAdded[] = $kit_cat_ID;
+							}
+
+							if (!empty($accessory_category)) {
+								$kit_cat_ID = $arrayOfCats[$accessory_category];
+								$TaxIDsToBeAdded[] = $kit_cat_ID;
+							}
+
+							if (!empty($lens_category)) {
+								$kit_cat_ID = $arrayOfCats[$lens_category];
+								$TaxIDsToBeAdded[] = $kit_cat_ID;
+							}
+
+							// GOT TO HERE.
+							// MAKING THE ARRAY OF IDS
+
+							echo "<h1>bosh:</h1>";
+							echo "<pre>";
+							print_r($TaxIDsToBeAdded);
+							echo "</pre>";
 
 							// SET POST TERM...
 							$post_id = $post->ID;
-							$taxonomy = 'kit_category'; 
+							$taxonomy = 'kit_category';  // people_category
 							echo "<pre>";
 							print_r($post_id);
 							echo "<br/>";
-							print_r($tag);
+							print_r($tags);
 							echo "<br/>";
 							print_r($taxonomy);
 							echo "</pre>";
 
-							// DO NOT UNCOMMENT THIS LINE TIL READY...
-							// wp_set_post_terms( $post_id, $tag, $taxonomy );
+							// DO NOT UNCOMMENT THIS UNLESS YOUR READY TO REPLACE ALL KIT TAXs...
+							// wp_set_post_terms( $post_id, $TaxIDsToBeAdded, $taxonomy );
 
 						?>
 			        <?php }
