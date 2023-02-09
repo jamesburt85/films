3fc07272c7ed16bef5aa78896aa1405dec69f814
Jay <hello@jburt.co.uk>
2023-02-09 12:54:55 +0000

    Footer URL

--- wp-content/themes/films/footer.php
+++ wp-content/themes/films/footer.php
@@ -11,7 +11,7 @@
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
 							</p>
 							<p>
-								<a href="<?php echo site_url(); ?>/contact">Contact</a>
+								<a href="<?php echo site_url(); ?>/find-us/">Contact</a>
 							</p>
 						</div>
 					</div>
